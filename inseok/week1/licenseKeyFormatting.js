44013f02b72c79e14fed1a134722fdb497537995
Inseok Seo <inseok.ucla@gmail.com>
2020-02-01 12:43:30 -0800

    week1 done

--- inseok/week1/licenseKeyFormatting.js
+++ inseok/week1/licenseKeyFormatting.js
@@ -3,12 +3,11 @@ var licenseKeyFormatting = function(S, K) {
   const arrayWithNoDash = S.split('-').join('');
   let counter = 0;
   for (let i = arrayWithNoDash.length - 1; i >= 0; i -= 1) {
-      array.push(arrayWithNoDash[i].toUpperCase());
-      counter += 1;
-      if (counter % K === 0 && i !== 0) {
-          array.push('-');
-      }
-      
+    array.push(arrayWithNoDash[i].toUpperCase());
+    counter += 1;
+    if (counter % K === 0 && i !== 0) {
+      array.push('-');
+    }
   }
   return array.reverse().join('')
 };
--- inseok/week1/uniqueEmail.js
+++ inseok/week1/uniqueEmail.js
@@ -1,5 +1,17 @@
-const test = () => {
-  console.log('test')
+var numUniqueEmails = function (emails) {
+  const map = {};
+  for (let i = 0; i < emails.length; i += 1) {
+    let email = '';
+    const atIndex = emails[i].indexOf('@');
+    const local = emails[i].slice(0, atIndex);
+    const domain = emails[i].slice(atIndex);
+    const plusIndex = local.indexOf('+');
+    if (plusIndex > 0) {
+      email = local.slice(0, plusIndex).split('.').join('') + domain;
+    } else {
+      email = local.split('.').join('') + domain
+    }
+    map[email] = true;
+  }
+  return Object.keys(map).length;
 };
-
-test();
