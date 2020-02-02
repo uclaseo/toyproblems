a57bffc8ccf7a366390cb5424b41cf465ff96a2a
Vivian Park <v2park@ucsd.edu>
2020-02-01 23:07:00 -0800

    week1 done

---
+++ vivian/week1/licenseKeyFormatting.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} S
+ * @param {number} K
+ * @return {string}
+ */
+var licenseKeyFormatting = function(S, K) {
+    // get key without hyphens
+    let key = S.replace(/-/g, '');
+
+    // calculate length of 1st group (should be at least 1)
+    let firstGroupLen = key.length % K == 0 ? K : key.length % K;
+
+    // init formatted with first group
+    let formatted = key.substring(0, firstGroupLen);
+
+    // keep track of number of chars in group
+    let groupCharCount = 0;
+
+    for (let i = firstGroupLen; i < key.length; i++) {
+        // if new group, concat hypen
+        if (groupCharCount == 0) formatted += '-';
+
+        formatted += key.charAt(i);
+
+        if (groupCharCount + 1 == K) groupCharCount = 0;
+        else groupCharCount++;
+    }
+    
+    return formatted.toUpperCase();
+};
+
+let S = "2-5g-3-J";
+let K = 2;
+console.log(licenseKeyFormatting(S, K));
---
+++ vivian/week1/validParentheses.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    if (s === null) return true;
+    if (s.length % 2 !== 0) return false;
+
+    let lookup = {
+        '(': ')',
+        '{': '}',
+        '[': ']',
+    };
+
+    let stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        let currChar = s.charAt(i);
+        if (lookup[currChar] == undefined) {
+            if (lookup[stack[stack.length-1]] == currChar) stack.pop();
+            else return false;
+        } else stack.push(currChar);
+    }
+
+    return stack.length == 0;
+};
+
+let s = "((";
+console.log(isValid(s));
