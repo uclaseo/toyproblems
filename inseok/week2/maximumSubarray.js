8ed49999a557460178bdc4f543d1cc8c1b251ad3
Inseok Seo <inseok.ucla@gmail.com>
2020-02-08 16:14:59 -0800

    Completed maximumSubarray - cheated

--- inseok/week2/maximumSubarray.js
+++ inseok/week2/maximumSubarray.js
@@ -12,3 +12,12 @@ var maxSubArray = function(nums) {
   }
   return maxSum;
 };
+
+var maxSubArray = function(nums) {
+  let maxSum = nums[0];
+  for (let i = 1; i < nums.length; i += 1) {
+    if (nums[i - 1] > 0) nums[i] += nums[i - 1];
+    maxSum = Math.max(nums[i], maxSum);
+  }
+  return maxSum;
+}
