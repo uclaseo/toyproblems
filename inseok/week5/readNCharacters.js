8f15332c3dc812fd51ed9fcc6fa27c95157dafca
Inseok Seo <inseok.ucla@gmail.com>
2020-02-29 19:21:30 -0800

    givenNCharacters

---
+++ inseok/week5/readNCharacters.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for read4()
+ * 
+ * @param {character[]} buf Destination buffer
+ * @return {number} The number of actual characters read
+ * read4 = function(buf) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} read4()
+ * @return {function}
+ */
+var solution = function(read4) {
+  /**
+   * @param {character[]} buf Destination buffer
+   * @param {number} n Number of characters to read
+   * @return {number} The number of actual characters read
+   */
+  return function(buf, n) {
+      while (buf.length < n) {
+          const nibble = [];
+          read4(nibble);
+          if (nibble.length === 0) break;
+          buf.push(...nibble);
+      }
+      buf.splice(n);
+      return buf.length;
+  };
+};
