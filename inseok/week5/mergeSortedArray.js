6e2001df40af349e52830ecfc2a64e0b19e14707
Inseok Seo <inseok.ucla@gmail.com>
2020-02-29 19:18:32 -0800

    mergeSortedArray

---
+++ inseok/week5/mergeSortedArray.js
@@ -0,0 +1,12 @@
+var merge = function (nums1, m, nums2, n) {
+  var len = m + n;
+  m--;
+  n--;
+  while (len--) {
+      if (n < 0 || nums1[m] > nums2[n]) {
+          nums1[len] = nums1[m--];
+      } else {
+          nums1[len] = nums2[n--];
+      }
+  }
+};
