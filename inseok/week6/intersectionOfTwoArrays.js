fd3f1e486ef07e051c66111b9bdb20d1d40a97b7
Inseok Seo <inseok.ucla@gmail.com>
2020-03-07 15:59:53 -0800

    intersectionOfTwoArrays

---
+++ inseok/week6/intersectionOfTwoArrays.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+  const map = {};
+  const result = [];
+  for (let i = 0; i < nums1.length; i += 1) {
+      map[nums1[i]] = true;
+  }
+  for (let j = 0; j < nums2.length; j += 1) {
+      if (map[nums2[j]]) {
+          result.push(nums2[j]);
+          map[nums2[j]] = false;
+      }
+  }
+  return result;
+};
