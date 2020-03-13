637a2767f8da08bc6917d8827fc80853e7c66a87
Inseok Seo <inseok.ucla@gmail.com>
2020-03-12 22:19:19 -0700

    Added more solutions

--- inseok/week7/movingAverageFromDataStream.js
+++ inseok/week7/movingAverageFromDataStream.js
@@ -26,6 +26,32 @@ MovingAverage.prototype.next = function(val) {
   }
 };
 
+MovingAverage.prototype.next = function(val) {
+  this.queue.push(val);
+  let sum = 0;
+  for (let i = Math.max(0, this.queue.length - this.size); i < this.queue.length; i += 1) {
+      sum += this.queue[i];
+  }
+  return sum / Math.min(this.size, this.queue.length);
+}
+
+MovingAverage.prototype.next = function(val) {
+  let popped = 0;
+  this.queue.push(val);
+  if (this.queue.length > this.size) {
+      popped = this.queue.shift();
+  }
+  this.windowSum += val - popped;
+  return this.windowSum / Math.min(this.queue.length, this.size);
+}
+MovingAverage.prototype.next = function(val) {
+  let tail = (this.head + 1) % this.size;
+  this.windowSum += val - (this.queue[tail] ? this.queue[tail] : 0);
+  this.head = (this.head + 1) % this.size;
+  this.queue[this.head] = val;
+  return this.windowSum / Math.min(this.queue.length, this.size);
+}
+
 /** 
 * Your MovingAverage object will be instantiated and called as such:
 * var obj = new MovingAverage(size)
